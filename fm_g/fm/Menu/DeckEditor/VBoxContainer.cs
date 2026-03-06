0f3b615e1f3db3a2f42dd70c5e462615a38033f8
leonardo <leonardo.rubert@edu.pucrs.br>
2026-03-06 15:42:43 -0300

    inicio do deck editor

---
+++ fm_g/fm/Menu/DeckEditor/VBoxContainer.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class VBoxContainer : Godot.VBoxContainer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var scene1 = "res://Menu/DeckEditor/SlotCarta.tscn";
+		// var scene1 = "res://Menu/FreeDuelCell2.tscn";
+		var scene = GD.Load<PackedScene>(scene1);
+		for(int i = 0; i < 40; i++)
+		{
+			var cell = scene.Instantiate();
+			//cell.Call("IrParaIndex", i);
+			AddChild(cell);
+		}
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
