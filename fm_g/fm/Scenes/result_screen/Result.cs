f6309b2f89a40f732889ac79f63fca90307be170
Leozin <yazucrubert@gmail.com>
2026-05-02 10:56:42 -0300

    adicionado tela de spoils na tela de result, para retorno visual das cartas adquiridas

--- fm_g/fm/Scenes/result_screen/Result.cs
+++ fm_g/fm/Scenes/result_screen/Result.cs
@@ -6,13 +6,22 @@ public partial class Result : Control
 {
 	public Control Screen {get;set;}
 	public List<Control> Screens;
+	public Godot.GridContainer Grid {get;set;}
+	public PackedScene CardUI = GD.Load<PackedScene>("res://Menu/Password/card_ui.tscn");
+
 	private int index = 0;
 	// Called when the node enters the scene tree for the first time.
+	//140/196 custom size pras cartas ficarem bem organizadas
 	public override void _Ready()
 	{
+		Grid = GetNode<Godot.GridContainer>("Screen/SpoilScreen/Spoils");
 		Screen = GetNode<Control>("Screen");
 		if(Screen != null)
 			Screens = Screen.GetChildren().Cast<Control>().ToList();
+		if(Grid != null)
+		{
+			InstanciaMao(new List<int>(){1,2,3,4,5,6,7,8,9,10,11,12,13,14,15});
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -52,6 +61,18 @@ public partial class Result : Control
 			GD.Print(item.Name);
 			RankMapper.ToNode(item, rank);
 		}		
+	}
+
+	public void InstanciaMao(List<int> CartasNaMaoLocal)
+	{
+		foreach(var item in CartasNaMaoLocal)
+		{
+			var cartaControlada = CardUI.Instantiate<CardUi>();
+			cartaControlada.index = item;
+			cartaControlada.CustomMinimumSize = new Vector2(140,196);
+			cartaControlada.Theme = GD.Load<Theme>("res://Resources/tema_carta_hand.tres");
+			Grid.AddChild(cartaControlada);	
+		}
 	}
 	
 }
